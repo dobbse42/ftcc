48e28e522729be18617c7643ff1b02a09bfa5a1e
dobbse42 <edjunk13@hotmail.com>
2026-02-11 14:00:20 +0200

    Moving things into dependency groups to allow for including layers with potentially conflicting dependencies

--- src/ftcc/__init__.py
+++ src/ftcc/__init__.py
@@ -1,9 +1,19 @@
+__all__ = ["MQTEncodingLayer", "PyZXLayer", "TopologiqLayer", "TQECLayer", "UCCLayer"]
+
 from .compilation_graph import (
     CompilationGraph as CompilationGraph,
 )
 
-from .compilation_layers.mqt_encoding_layer import MQTEncodingLayer
 
-from .compilation_layers.pyzx_layer import PyZXLayer
-from .compilation_layers.topologiq_layer import TopologiqLayer
-from .compilation_layers.tqec_layer import TQECLayer
+def __getattr__(name):
+    if name == "MQTEncodingLayer":
+        from .compilation_layers.mqt_encoding_layer import MQTEncodingLayer
+    if name == "PyZXLayer":
+        from .compilation_layers.pyzx_layer import PyZXLayer
+    if name == "TopologiqLayer":
+        from .compilation_layers.topologiq_layer import TopologiqLayer
+    if name == "TQECLayer":
+        from .compilation_layers.tqec_layer import TQECLayer
+    if name == "UCCLayer":
+        from .compilation_layers.ucc_layer import UCCLayer
+    raise AttributeError(f"module {__name__} has no attribute {name}")
